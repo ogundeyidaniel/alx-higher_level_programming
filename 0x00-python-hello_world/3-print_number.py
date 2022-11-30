8fd1a995dcdca6c9a599b00a0080b6811e665a07
ogundeyidaniel <ogundeyidaniel@outlook.com>
2022-11-30 15:37:42 +0000

    Task 03

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-number = 98
-print("{:d} Battery street".format(number))
+for ch in range(97, 123):
+        if ch != 101 and ch != 113:
+                print("{:c}".format(ch), end='')
