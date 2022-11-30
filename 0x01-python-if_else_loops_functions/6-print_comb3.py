3a5d4cb94592cfa77678b25f7f267e7bb07d2d1f
ogundeyidaniel <ogundeyidaniel@outlook.com>
2022-11-30 14:29:03 +0000

    Task 07

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(10):
-        if j > i and i is not 8:
-            print("{}{}".format(i, j), end=", ")
-print("89")
+for digit1 in range(0, 9):
+    for digit2 in range(digit1 + 1, 10):
+        if digit1 == 8:
+            print("{:d}{:d}".format(digit1, digit2))
+            break
+        print("{:d}{:d}".format(digit1, digit2), end=", ")
