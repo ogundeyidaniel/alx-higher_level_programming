2fbad14223854f25732478057117bca7dd592a14
ogundeyidaniel <ogundeyidaniel@outlook.com>
2022-12-01 23:48:57 +0000

    Task 06

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,23 +1,15 @@
 #!/usr/bin/python3
-from sys import argv
-from calculator_1 import add, sub, mul, div
-if __name__ != "__main__":
-    exit()
-
-argc = len(argv) - 1
-if argc != 3:
-    print("Usage: {:s} <a> <operator> <b>".format(argv[0]))
-    exit(1)
-elif argv[2] == '+':
-    result = add(int(argv[1]), int(argv[3]))
-elif argv[2] == '-':
-    result = sub(int(argv[1]), int(argv[3]))
-elif argv[2] == '*':
-    result = mul(int(argv[1]), int(argv[3]))
-elif argv[2] == '/':
-    result = div(int(argv[1]), int(argv[3]))
-else:
-    print("Unknown operator. Available operators: +, -, * and /")
-    exit(1)
-
-print("{:s} {:s} {:s} = {:d}".format(argv[1], argv[2], argv[3], result))
+if __name__ == '__main__':
+    from calculator_1 import add, sub, mul, div
+    import sys
+    args, arg_len = sys.argv, len(sys.argv) - 1
+    table = {"+": add, "-": sub, "*": mul, "/": div}
+    if arg_len != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    if args[2] not in "+-*/":
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    a, op, b = int(args[1]), args[2], int(args[3])
+    c = table[args[2]](a, b)
+    print("{} {} {} = {}".format(a, op, b, c))
