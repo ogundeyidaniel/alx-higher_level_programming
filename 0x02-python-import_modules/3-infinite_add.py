4dd686b367cb17f47cf1a4f6dcc6e4dd894e6e4d
ogundeyidaniel <ogundeyidaniel@outlook.com>
2022-12-01 23:18:33 +0000

    Task 03

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-import sys
 
-if __name__ != "__main__":
-    exit()
+if __name__ == "__main__":
+    """Print the addition of all arguments."""
+    import sys
 
-i = 0
-result = 0
-for argument in sys.argv:
-    if i != 0:
-        result += int(argument)
-    else:
-        i += 1
-print("{:d}".format(result))
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
