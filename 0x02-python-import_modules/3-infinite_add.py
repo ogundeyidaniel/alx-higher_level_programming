937af3a0771d85846b01844dcebe6204ae9b4059
ogundeyidaniel <ogundeyidaniel@outlook.com>
2022-12-01 23:14:30 +0000

    Task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
-    import sys
-    print(sum(int(i) for i in sys.argv[1:]))
+import sys
+
+if __name__ != "__main__":
+    exit()
+
+i = 0
+result = 0
+for argument in sys.argv:
+    if i != 0:
+        result += int(argument)
+    else:
+        i += 1
+print("{:d}".format(result))
