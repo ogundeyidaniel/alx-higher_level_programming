1c28373ccd2b02e600598c585813e2879e315f9f
ogundeyidaniel <ogundeyidaniel@outlook.com>
2022-12-02 12:28:54 +0000

    Task 06

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i in row:
+            print("{:d}".format(i), end=" " if row[-1] != i else "")
+        print()
