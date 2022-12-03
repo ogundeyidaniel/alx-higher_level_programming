bd7ede3363fa580c3ced32e01655c8d8821d559e
ogundeyidaniel <ogundeyidaniel@outlook.com>
2022-12-03 03:40:10 +0000

    Task 02

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-from sys import argv
-
 if __name__ == "__main__":
-    ac = len(argv) - 1
-    if ac == 0:
-        print("0 arguments.")
-    elif ac == 1:
-        print("1 argument:")
-    else:
-        print("{} arguments:".format(ac))
-    for i in range(1, ac + 1):
-        print("{}: {}".format(i, argv[i]))
+    import sys
+    lst = sys.argv
+    lth = len(lst) - 1
+    arg = "argument"
+    print("{} argument{}{}".format(
+            lth,
+            "" if lth == 1 else "s",
+            "." if lth == 0 else ":"))
+    for i in range(1, len(lst)):
+        print("{}: {}".format(i, lst[i]))
