640d369dec2de5c95ef14fe36c8da35babfae5e1
ogundeyidaniel <ogundeyidaniel@outlook.com>
2022-12-03 03:48:19 +0000

    Task 02

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
 
-from sys import argv
-
-
 if __name__ == "__main__":
+     """Print the number of and list of arguments."""
+      import sys
 
-    nargs = len(argv) - 1
-
-    if nargs == 0:
-        print("0 arguments.")
-    elif nargs == 1:
-        print("1 argument:")
-    else:
-        print("{} arguments:".format(nargs))
-
-    for i, v in enumerate(argv[1:]):
-        print("{}: {}".format(i + 1, v))
+       count = len(sys.argv) - 1
+       if count == 0:
+           print("0 arguments.")
+            else:
+                print("{} arguments:".format(count))
+                for i in range(count):
+                    print("{}: {}".format(i + 1, sys.argv[i + 1]))
