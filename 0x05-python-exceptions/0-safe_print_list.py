a389c0add72d21f59ac35eb13877cb797098a4d2
https://github.com/ogundeyidaniel <ogundeyidaniel@outlook.com>
2022-12-19 20:13:16 -0800

    Python Exceptions

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    elements = 0
+    for i in range(0, x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            elements += 1
+        except IndexError:
+            break
+    print("")
+    return elements
