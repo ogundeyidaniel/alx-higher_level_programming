8882aa259f84ac260652c496763e2a9f82a0506e
https://github.com/ogundeyidaniel <ogundeyidaniel@outlook.com>
2022-12-21 20:05:45 -0800

    Task 01

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,17 @@
+#!/usr/bin/python3
+"""Square module"""
 
+
+class Square:
+    """
+    Square define a geometric shape square
+    Attributes:
+        size (int): the size of the square
+    """
+    def __init__(self, size):
+        """
+        Init method is a constructor fo Square class
+        Args:
+            size (int): the size of the square
+        """
+        self.__size = size
