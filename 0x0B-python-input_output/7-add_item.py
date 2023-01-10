bacafbf196a41d8f9d40c50e9077343c55526dc6
https://github.com/ogundeyidaniel <ogundeyidaniel@outlook.com>
2023-01-10 06:19:28 -0800

    Python-Input & Output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
-"""
-adds all arguments to a Python list, and then save them to a file
-"""
-import sys
+import json
 
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
-open("add_item.json", "a")
-try:
-    l = load_from_json_file("add_item.json")
-except ValueError:
-    l = []
-save_to_json_file(l + sys.argv[1:], "add_item.json")
+def save_to_json_file(my_obj, filename):
+    """
+    Function that returns the JSON representation of an object (string)
+    """
+    with open(filename, mode='w') as my_json:
+        json.dump(my_obj, my_json)
