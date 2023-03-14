5780c32dc7bbc2b2fc1ac5bce46e8528b8e6a4fb
https://github.com/ogundeyidaniel <ogundeyidaniel@outlook.com>
2023-03-13 19:22:12 -0700

    0-javascript_is_amazing.js

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,3 +1,3 @@
-#!/usr/bin/node
+#!/usr/bin/mode
 const myVar = 'JavaScript is amazing';
 console.log(myVar);
--- 0x12-javascript-warm_up/1-multi_languages.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/mode
-console.log('C is fun');
-console.log('Python is cool');
-console.log('Javascript is amazing');
