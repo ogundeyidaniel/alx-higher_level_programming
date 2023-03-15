0e93003dba91f8bd04c7bfc793133e48b0c2c02f
https://github.com/ogundeyidaniel <ogundeyidaniel@outlook.com>
2023-03-15 14:58:13 -0700

    sort

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,16 +1,11 @@
-<<<<<<< HEAD
-
-=======
 #!/usr/bin/node
 const dict = require('./101-data').dict;
 const newDict = {};
-
-Object.keys(dict).map(function (key, index) {
-  if (newDict[dict[key]] === undefined) {
-    newDict[dict[key]] = [];
+for (const [key, value] of Object.entries(dict)) {
+  if (newDict[value] !== undefined) {
+    newDict[value].push(key);
+  } else {
+    newDict[value] = [key];
   }
-  newDict[dict[key]].push(key);
-});
-
+}
 console.log(newDict);
->>>>>>> 4257823b8176f3d9b1b0cc31d22832b83e6c9295
