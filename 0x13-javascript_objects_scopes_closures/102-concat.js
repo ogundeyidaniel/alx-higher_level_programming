6546e4d23096344bafa37784a9a0d9cef239540f
https://github.com/ogundeyidaniel <ogundeyidaniel@outlook.com>
2023-03-15 14:16:37 -0700

    javascript

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-const f1 = fs.readFileSync(process.argv[2], 'utf8');
-const f2 = fs.readFileSync(process.argv[3], 'utf8');
-fs.writeFileSync(process.argv[4], f1 + f2, 'utf-8');
+const a = fs.readFileSync(process.argv[2], 'utf8');
+const b = fs.readFileSync(process.argv[3], 'utf8');
+fs.writeFileSync(process.argv[4], a + b);
