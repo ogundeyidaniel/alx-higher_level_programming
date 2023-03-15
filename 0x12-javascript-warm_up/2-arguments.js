ad56bbad21c24238cfef1b982ae88f67e8cb4ac5
Ogundeyi Daniel Oluwatunmise <111064043+ogundeyidaniel@users.noreply.github.com>
2023-03-15 01:24:55 +0100

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,7 @@
 #!/usr/bin/node
-let x = 0;
-process.argv.forEach((_val, _index) => {
-  x++;
-});
-if (x <= 2) {
-  console.log('No argument');
-} else if (x === 3) {
-  console.log('Argument found');
-} else {
-  console.log('Arguments found');
-}
+if (process.argv.length < 3) 
+{console.log('No argument');}
+else if (process.argv.length === 3)
+{console.log('Argument found');}
+else
+{console.log('Arguments found');}
