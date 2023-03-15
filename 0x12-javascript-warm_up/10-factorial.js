45e1b005ad482f0dd97e07687f28693f3c5c38bb
https://github.com/ogundeyidaniel <ogundeyidaniel@outlook.com>
2023-03-15 16:31:11 -0700

    print

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
-
-function factorialize (num) {
-  let MyRes = 1;
-  for (let i = 1; i <= num; i++) {
-    MyRes *= i;
-  }
-  return (MyRes);
+function factorial (n) {
+  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
 }
-console.log(factorialize(parseInt(process.argv[2])));
+
+console.log(factorial(Number(process.argv[2])));
