8c6a78ca18c9f8038e6193007a705db680ee355f
Ogundeyi Daniel Oluwatunmise <111064043+ogundeyidaniel@users.noreply.github.com>
2023-04-27 02:35:08 +0100

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
-const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (err, contents) {
-  if (contents === undefined) {
-    console.log(err);
+// reads and prints the content of a file.
+const file = process.argv[2];
+const fileStream = require('fs');
+fileStream.readFile(file, 'utf-8', (error, data) => {
+  if (error) {
+    console.log(error);
   } else {
-    console.log(contents);
+    console.log(data);
   }
 });
