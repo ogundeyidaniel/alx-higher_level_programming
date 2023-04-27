f35ba600b0b933cd1ee76aad4e15b0c83db9a117
Ogundeyi Daniel Oluwatunmise <111064043+ogundeyidaniel@users.noreply.github.com>
2023-04-27 05:32:14 +0100

    1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,3 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.writeFileSync(process.argv[2], process.argv[3]);
+const filename = process.argv[2];
+const content = process.argv[3];
+fs.writeFile(filename, content, 'utf-8', (error) => {
+  if (error) {
+    console.log(error);
+  }
+});
